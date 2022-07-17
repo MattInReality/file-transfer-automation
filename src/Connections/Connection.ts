58d4b5cca87d10235e891d19f602c5fd57aeeea6
MattInReality <mattnorth84@gmail.com>
2022-07-17 21:09:12 +0100

    ABC for connection API.

---
+++ src/Connections/Connection.ts
@@ -0,0 +1,19 @@
+import { Readable, Writable } from "stream";
+
+export abstract class Connection {
+  abstract _open(): void;
+
+  abstract _close(): void;
+
+  abstract download(remotePath: string, to: Writable): Promise<void>;
+
+  abstract upload(localPath: Readable, to: string): Promise<void>;
+}
+
+export interface ConnectionOptions {
+  host?: string;
+  port?: number;
+  username?: string;
+  password?: string;
+  secure?: boolean;
+}
