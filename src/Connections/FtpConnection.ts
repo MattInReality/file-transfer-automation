7063e8b8cb90ebac73de32ca9bc19401cfce02c4
MattInReality <mattnorth84@gmail.com>
2022-07-23 21:26:27 +0100

    Fixed the implementation of the FTP Connection as the underlying module annoyingly uses different property names in the connection options.

--- src/Connections/FtpConnection.ts
+++ src/Connections/FtpConnection.ts
@@ -1,30 +1,62 @@
 import { Connection, ConnectionOptions } from "./Connection";
 import * as ftp from "basic-ftp";
-import { ReadStream, WriteStream } from "fs";
 import type { Client } from "basic-ftp";
+import { Readable, Writable } from "stream";
 
 export class FtpConnection implements Connection {
   private client: Client;
   private readonly connectionOptions: ConnectionOptions;
+  private readonly user: string | undefined;
+  private readonly password: string | undefined;
+  private readonly secure: boolean | undefined;
+  private readonly host: string | undefined;
 
   constructor(connectionOptions: ConnectionOptions) {
     this.client = new ftp.Client();
     this.connectionOptions = connectionOptions;
+    this.user = connectionOptions.username;
+    this.password = connectionOptions.password;
+    this.host = connectionOptions.host;
+    this.secure = connectionOptions.secure;
+    // this.client.ftp.verbose = true;
   }
 
-  _open = async () => await this.client.access(this.connectionOptions);
-
-  _close = async () => this.client.close();
-
-  download = async (remotePath: string, to: WriteStream): Promise<void> => {
-    await this._open();
-    await this.client.downloadTo(to, remotePath);
-    return await this._close();
+  _open = async () => {
+    await this.client.access({
+      host: this.host,
+      user: this.user,
+      password: this.password,
+      secure: this.secure,
+    });
   };
 
-  upload = async (localPath: ReadStream, to: string): Promise<void> => {
-    await this._open();
-    await this.client.uploadFrom(localPath, to);
-    return await this._close();
+  _close = async () => {
+    return this.client.close();
+  };
+
+  download = async (remotePath: string, to: Writable): Promise<void> => {
+    try {
+      await this._open();
+      await this.client.downloadTo(to, remotePath);
+      return;
+    } catch (e: any) {
+      console.error("ftpConnection download error!");
+      throw new Error(e.message);
+    } finally {
+      await this._close();
+    }
+  };
+
+  upload = async (localPath: Readable, to: string): Promise<void> => {
+    try {
+      await this._open();
+      await this.client.uploadFrom(localPath, to);
+      return;
+    } catch (e: any) {
+      console.error("ftpConnection upload error!");
+      throw new Error(e.message);
+    } finally {
+      await this._close();
+    }
   };
 }
