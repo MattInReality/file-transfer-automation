6c06f9cccf0323ad0fd5e21ee5f1401cc4070605
MattInReality <mattnorth84@gmail.com>
2022-08-07 21:33:17 +0100

    Abstracted creating the breeJob into a function. Realised that this is all just the result of local data problems and will be removed.

--- src/Scheduling/scheduler.ts
+++ src/Scheduling/scheduler.ts
@@ -6,29 +6,42 @@ import { getPathRelativeToRoot } from "../helpers.js";
 
 const root = fileURLToPath(new URL("../jobs", import.meta.url).toString());
 
+//TODO: Read up on and add Graceful
+// https://github.com/forwardemail/forwardemail.net/blob/master/bree.js
+// https://github.com/ladjs/graceful
 export const scheduler = new Bree({
   root,
   worker: { env: SHARE_ENV },
   logger: appLogger,
 });
 
+//Supposed to be a basic structure for job creation but arguably unnecessary.
+//TODO: Use this as guidance for Bree job creation for db storage.
 export interface AppJob {
   jobType: string;
   jobDataId: number;
-  cron?: string;
+  cron: string;
   timeout?: number;
+  interval?: number;
 }
 
+//A bree job isn't the same and so I need to create a Bree Job from my job. Particular focus on the path...
+//TODO: Is this a local data issue? Create the Bree job at input instead of parsing it.
+const convertToBreeJob = (job: AppJob) => {
+  return {
+    name: `${job.jobType}-${job.jobDataId}`,
+    path: getPathRelativeToRoot(`/build/jobs/${job.jobType}.js`),
+    timeout: job.timeout ?? 0,
+    cron: job.cron,
+    worker: {
+      workerData: job.jobDataId,
+    },
+  };
+};
+
+//TODO: When we move to storing jobs in DB don't forget to change the types here and bin off the conversion.
 export const initJobs = async (jobs: AppJob[], scheduler: Bree) => {
   for (const job of jobs) {
-    await scheduler.add({
-      name: `${job.jobType}-${job.jobDataId}`,
-      path: getPathRelativeToRoot(`/build/jobs/${job.jobType}.js`),
-      cron: job.cron || undefined,
-      timeout: job.timeout || undefined,
-      worker: {
-        workerData: job.jobDataId,
-      },
-    });
+    await scheduler.add(convertToBreeJob(job));
   }
 };
