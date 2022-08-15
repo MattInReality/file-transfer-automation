6975db93c1ec28ef20cee8902af4fc12a06909e2
MattInReality <mattnorth84@gmail.com>
2022-08-15 12:53:47 +0100

    Added a method to create the init object for a request. It allows for bearer tokens but doesn't have a content type feature yet.

--- src/Connections/HTTPConnection.ts
+++ src/Connections/HTTPConnection.ts
@@ -1,12 +1,18 @@
 import { Connection, ConnectionOptions } from "./Connection.js";
-import fetch from "node-fetch";
+import fetch, { Headers, RequestInit } from "node-fetch";
 import { Readable, Writable } from "stream";
 import { pipeline } from "stream/promises";
 import path from "path";
+
+type requestMethods = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
 export class HTTPConnection implements Connection {
   private readonly baseUrl: string;
+  private readonly apiKey: string | undefined;
+
   constructor(private readonly connectionOptions: ConnectionOptions) {
     this.baseUrl = connectionOptions.host;
+    this.apiKey = connectionOptions.apiKey;
   }
 
   _open() {}
@@ -15,24 +21,58 @@ export class HTTPConnection implements Connection {
 
   download = async (remotePath: string, to: Writable): Promise<void> => {
     //TODO: expand upon the request to add authentication properties and headers
-    const response = await fetch(path.join(this.baseUrl, remotePath));
+    const endpoint = path.join(this.baseUrl, remotePath);
+    const initOptions = this.createRequestInit("GET");
+    const response = await fetch(endpoint, initOptions);
     if (!response.ok || response.body === null) {
-      throw new Error(`Error downloading from remote: ${remotePath}`);
+      throw new Error(
+        `Error downloading from remote: ${endpoint} - STATUS: ${response.status}`
+      );
     }
 
     return pipeline(response.body, to);
   };
 
   upload = async (localPath: Readable, to: string): Promise<void> => {
-    const url = `${this.baseUrl}${to}`;
-    const response = await fetch(url, {
-      method: "POST",
-      body: localPath,
-    });
+    /*This is just a readStream of content now. It's not a multipart file transmission.
+      Dependent on how the end point receives the file.
+      future version will have more configuration options.
+    */
+    const endpoint = path.join(this.baseUrl, to);
+    const initOptions = this.createRequestInit("POST", localPath);
+    const response = await fetch(endpoint, initOptions);
     if (!response.ok) {
       throw new Error(
-        `Error uploading to remote: ${url} - STATUS: ${response.status} - RESPONSE: ${response.body}`
+        `Error uploading to remote: ${endpoint} - STATUS: ${response.status}`
       );
     }
+  };
+
+  createRequestInit = (
+    method: requestMethods = "GET",
+    bodyContent: string | Readable = ""
+  ) => {
+    const options: RequestInit = {
+      method: method.toUpperCase(),
+    };
+    // Body should only exist on a get method
+    if (bodyContent && method !== "GET") {
+      options.body = bodyContent;
+    }
+    //Future version needs content type.
+    if (!this.apiKey) {
+      return options;
+    }
+
+    //Future version will allow for both bearer and url string. For now... I'm sorry.
+    const headers = new Headers();
+    if (this.apiKey && this.apiKey.length > 0) {
+      headers.append(
+        "Authorization",
+        `Bearer ${this.connectionOptions.apiKey}`
+      );
+    }
+    options.headers = headers;
+    return options;
   };
 }
