03161951a40de6087e201230b4e090a09e98bb23
MattInReality <mattnorth84@gmail.com>
2022-09-04 20:53:41 +0100

    Tweaks to config because typescript threw an error on compile.

--- prisma/queries.ts
+++
@@ -1,7 +0,0 @@
-import { jobDB } from "../src/transferJobs.temp.js";
-// jobDB is temporary. Real prisma implementation required
-import { Transfer } from "../src/Transfer/TransferBroker.js";
-
-export const getTransferJobById = (id: number): Transfer | undefined => {
-  return jobDB.find((j: Transfer) => j.id === id);
-};
