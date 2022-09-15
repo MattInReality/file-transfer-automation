2f9dad1a8f5ea4045ebab43f34de16c3a5b2ffe3
MattInReality <mattnorth84@gmail.com>
2022-09-15 21:33:49 +0100

    Removed unused code, references and files from recent move to DB.

--- src/Scheduling/scheduler.ts
+++
@@ -1,7 +0,0 @@
-import { jobs } from "../transferJobs.temp";
-import { RequiredJobParams } from "./Job";
-
-//TODO: This when the prisma is attached this needs to query the prisma for the jobs.
-export const getJobs = async (): Promise<RequiredJobParams[]> => {
-  return Promise.resolve(jobs);
-};
